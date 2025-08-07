0c68074b0cf535b8a1192cebe199332a7d848478
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2025-08-07 18:33:33 -0300

    Hello World in PHP

---
+++ php/exe0000/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Primeiro Código PHP</h1>
+    <?php
+      echo "Hello, World! \u{1f30e}";
+    ?>
+</body>
+</html>
