da4da68165c0067c27085bffd0b80783f41e98bb
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2025-08-13 17:57:28 -0300

    Update index.php

--- php/exe0006/index.php
+++ php/exe0006/index.php
@@ -1,18 +1,26 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Números Randômicos</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 <body>
+    <header>
+        <h1>Obtenha um número entre 0 e 100</h1>
+    </header>
 
-<?php
-$min = 0;
-$max = 100;
-$num = mt_rand($min, $max);
+    <?php
+      $min = 0;
+      $max = 100;
+      $num = mt_rand($min, $max);
 
-echo "Gerando um número aleatório entre $min e $max ... <br />";
+      echo "Gerando um número aleatório entre $min e $max ... <br />";
+      echo "O número é $num <br />";
+    ?>
 
-echo "O número é $num <br />";
-?>
-
-<button onclick="javascript:document.location.reload()">&#x1F504;Gerar outro número</button>
+    <button onclick="javascript:document.location.reload()">&#x1F504;Gerar outro número</button>
 
 </body>
 </html>
