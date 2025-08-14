019549f30fdd273423d36328a9df4b398e7f5b92
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2025-08-14 12:33:55 -0300

    Update index.php

--- php/exe0007/index.php
+++ php/exe0007/index.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 <body>
-
-<?php
-$inicio = date("m-d-Y", strtotime("-7 days"));
-$fim = date("m-d-Y");
-$url='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
-
-$dados = json_decode(file_get_contents($url),true);
-//var_dump($dados);
-$cotacao = dados["value"][0]["cotacaoCompra"];
-
-echo "A cotação do dólar foi $cotacao";
-
-?> 
-
+    <header>
+        <h1>Informe o valor em Reais para converter em Dólares</h1>
+    </header>
+    <main>
+      <h1>Conversor BRL-USD</h1>
+      <form action="result.php" method="get">
+        <label for="din">Qual o valor a ser convertido?</label>
+        <input type="number" name="din" id="din" step="0.01"/>
+        <input type="submit" value="converter" />
+      </form>
+    </main>
 </body>
 </html>
