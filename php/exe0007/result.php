4cffd5e069b9a3c4916f560901f53352938a18e5
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2025-08-15 10:17:40 -0300

    Update result.php

--- php/exe0007/result.php
+++ php/exe0007/result.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
+<html>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+    <title>Conversor</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
     <main>
-    <?php 
-      $cotacao = 5.39;
-      $real = $_GET["din"] ?? 101;
-      $dolar = $real / $cotacao;
-      $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
-      echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem à " . numfmt_format_currency($padrao, $dolar, "USD") . "<br />";
-    ?>
-    <button onclick="javascript:history.go(-1)">Voltar</button>
-    </main>
-</body>
+	  <h1>Convertendo de BRL para USD</h1>
+      <?php
+        $inicio = date("m-d-Y", strtotime("-7 days"));
+        $fim = date("m-d-Y");
+        $url='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
 
+        $dados = json_decode(file_get_contents($url),true);
+        //var_dump($dados);
+        $cotacao = $dados["value"][0]["cotacaoCompra"];
+
+        $real = $_GET["din"] ?? 101;
+        $dolar = $real / $cotacao;
+        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+        echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem à " . numfmt_format_currency($padrao, $dolar, "USD") . "<br />";
+      ?>
+      <button onclick="javascript:history.go(-1)">Voltar</button>
+    </main>
+  </body>
 </html>
